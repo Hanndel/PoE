760e26074cf4ec3f49dd0f859e5c8a86d3363f81
Hanndel <lnote@outlook.es>
2021-01-13 22:11:15 +0100

    Adding GUI

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,14 @@
-import java.awt.event.*;  
-import javax.swing.*; 
-import cosas.Breach;
+//import java.awt.event.*;  
+//import javax.swing.*; 
+//import cosas.Breach;
+import GUI.MainG;
+import GUI.BreachG;
+
 public class Main {
-    public static void main(String[] args) { 
-    JButton b=new JButton();//creating instance of JButton
-    b.setIcon(new ImageIcon("Img\\Breach\\Chayula\\ChayulasPureBreachstone.png"));
-    b.setBounds(0,0,60,40);//x axis, y axis, width, height
-    Breach breach = new Breach();
-
-    System.out.println(breach.a);
-
-    JFrame f=new JFrame();//creating instance of JFrame
-    final JTextField ct=new JTextField();
-    ct.setBounds(65,10, 150,20);
-
-    b.addActionListener(new ActionListener(){
-        public void actionPerformed(ActionEvent e){  
-            ct.setText("Welcome to Javatpoint.");  
-        }  
-    });
-    f.add(b);//adding button in JFrame
-    f.add(ct);
-    f.setSize(400,500);//400 width and 500 height  
-    f.setLayout(null);//using no layout managers  
-    f.setVisible(true);//making the frame visible  
+    public static void main(String[] args) {
+        MainG MainG = new MainG();
+        BreachG BreachG = new BreachG();
+        //MainG.main();
+        System.out.println("a");
     } 
 }  
